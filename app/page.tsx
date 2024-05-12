5a82ad0042987a22bcded18a4d453150f44464e8
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-05-12 05:49:11 +0200

    fix image animation

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@
 import DunePicture from '@/public/dune.jpg';
 import Button from '@/components/Button/Button';
 import { MotionImage } from '@/components/MotionImage/MotionImage';
-import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
+import {
+    motion,
+    useScroll,
+    useTransform,
+    spring,
+    circOut,
+} from 'framer-motion';
 import { useRef } from 'react';
 
 export default function Home() {
@@ -14,36 +20,40 @@ export default function Home() {
         layoutEffect: false,
     });
 
-    const imageY = useTransform(scrollYProgress, [0, 1], ['0', '10%']);
-    const headingY = useTransform(scrollYProgress, [0, 1], ['0', '-25%']);
+    const objectPosition = useTransform(
+        scrollYProgress,
+        [0, 1],
+        ['50% 10%', '50% 0%']
+    ).get();
+    const y = useTransform(scrollYProgress, [0, 1], ['0', '-25%']);
 
     return (
         <main
             ref={ref}
             className='text-primary flex h-full max-h-fit w-full flex-col'
         >
-            <section className='h-[90dvh] w-full p-20'>
+            <section className='relative h-[90dvh] w-full p-20'>
+                <motion.h1
+                    className='absolute bottom-[50px] left-20 z-10 select-none text-[224px] font-semibold leading-none'
+                    style={{ y }}
+                >
+                    Panowie
+                </motion.h1>
                 <div className='flex h-full w-full flex-row items-center justify-between'>
-                    <div className='mx-8 mb-12 flex w-full flex-col items-center justify-center'>
-                        <div className='flex flex-col gap-6'>
+                    <div className='mx-8 mb-12 flex w-full flex-col items-start justify-center'>
+                        <div className='ml-24 flex flex-col gap-6'>
                             <h2 className='w-fit min-w-72 whitespace-pre-line text-[19px] font-medium leading-7'>
                                 We are Panowie Pro. <br />A software studio for
                                 creatives.
                             </h2>
                             <Button>Work with us</Button>
                         </div>
-                        <motion.h1
-                            className='absolute bottom-[calc(10dvh+50px)] left-20 z-10 select-none text-[224px] font-semibold leading-none'
-                            style={{ y: headingY }}
-                        >
-                            Panowie
-                        </motion.h1>
                     </div>
                     <MotionImage
                         src={DunePicture}
                         alt='Picture of a sand dune with blue cloudless sky beyond.'
-                        className='relative h-full max-h-screen w-full object-cover object-[50%_25%]'
-                        style={{ y: imageY }}
+                        className='relative h-screen max-h-full w-full object-cover'
+                        style={{ objectPosition }}
                     />
                 </div>
             </section>
